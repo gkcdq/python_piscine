799b0ba325d6cdc86aa1a9ad028613a06d33e438
Thomas Milin <tmilin@f4r11s7.clusters.42paris.fr>
2026-03-27 05:52:44 +0100

    42

--- Python0/ex08/Loading.py
+++ Python0/ex08/Loading.py
@@ -1,2 +1,15 @@
 import os
 
+
+def ft_tqdm(lst: range) -> any:
+    total = len(lst)
+    for i, item in enumerate(lst):
+
+        cols = os.get_terminal_size().columns
+        bar_size = cols - 30
+        progress = (i + 1) / total
+        filled = int(bar_size * progress)
+        bar = "=" * filled + ">" + " " * (bar_size - filled)
+        output = f"\r{int(progress * 10)}%|[{bar}]| {i + 1}/{total}"
+        os.write(1, output.encode())
+        yield item
